4032bddbbe04f39ebd0ce0cf49a6621032b284be
Joseph <fruitfuljoseph@gmail.com>
2020-07-13 13:40:13 +0530

    Finished AssertionError for invalid extensions

--- transcribe.py
+++ transcribe.py
@@ -4,11 +4,18 @@ import os, sys
 import requests, json
 
 class Transcribe:
-	def __init__(self):
-		pass
+	def __init__(self,filename):
+		self.filename = filename
 
-	def identifyFormat():
-		pass
+	def identifyFormat(self):
+		valid_extensions = ('.mp3','.ogg','.wav','.m4a','.flac')
+		file_path, file_extension = os.path.splitext(self.filename)
+		if file_extension in valid_extensions:
+			# print('Valid')
+			pass
+		else:
+			error = 'File extension ' + file_extension + ' not valid!'
+			raise AssertionError(error)
 
 	def request():
 		pass
@@ -18,8 +25,11 @@ class Transcribe:
 
 
 if __name__ == "__main__":
-	fname = sys.argv[1]
-	print(fname)
+	file_name = sys.argv[1]
+
+
+	obj = Transcribe(file_name)
+	obj.identifyFormat()
 
 
 	
