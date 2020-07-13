d43b08247b9d226293da91e5d8b8c1dcdfe28c81
Joseph <fruitfuljoseph@gmail.com>
2020-07-13 14:37:01 +0530

    Finished identifyformat function

--- transcribe.py
+++ transcribe.py
@@ -7,14 +7,17 @@ class Transcribe:
 	def __init__(self,filename):
 		self.filename = filename
 
+	# Validates the file, checks for the valid file extension and returns audio-format
 	def identifyFormat(self):
 		valid_extensions = ('.mp3','.ogg','.wav','.m4a','.flac')
 		file_path, file_extension = os.path.splitext(self.filename)
 		if file_extension in valid_extensions:
-			# print('Valid')
-			pass
+			return file_extension[1:]
+		elif not file_extension:
+			error = file_name + file_extension + ' is either a directory or not a valid file'
+			raise AssertionError(error)
 		else:
-			error = 'File extension ' + file_extension + ' not valid!'
+			error = 'File extension ' + file_extension + ' not valid'
 			raise AssertionError(error)
 
 	def request():
@@ -29,7 +32,13 @@ if __name__ == "__main__":
 
 
 	obj = Transcribe(file_name)
-	obj.identifyFormat()
+	
+	audio_format = obj.identifyFormat()
+	print(audio_format)
+	#response = obj.request(audio_format)
+
+	#obj.printResponse(response)
+
 
 
 	
