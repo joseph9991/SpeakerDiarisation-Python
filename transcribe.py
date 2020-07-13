5c29c9414e0e71d55e0ebe14b860dfd704284a03
Joseph <fruitfuljoseph@gmail.com>
2020-07-13 13:18:25 +0530

    Renamed file & Added Skeleton for Transcribe Class

---
+++ transcribe.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os, sys
+import requests, json
+
+class Transcribe:
+	def __init__(self):
+		pass
+
+	def identifyFormat():
+		pass
+
+	def request():
+		pass
+
+	def printResponse():
+		pass
+
+
+if __name__ == "__main__":
+	fname = sys.argv[1]
+	print(fname)
+
+
+	
