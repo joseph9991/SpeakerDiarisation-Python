b2f13a1eb08cf42ffcf13f0d03d78f09d5d0d613
Joseph <fruitfuljoseph@gmail.com>
2020-07-13 16:13:00 +0530

    Finished printResponse function; Working for audio-file

--- transcribe.py
+++ transcribe.py
@@ -22,6 +22,7 @@ class Transcribe:
 			error = 'File extension ' + file_extension + ' not valid'
 			raise AssertionError(error)
 
+	# Opens the File, sets headers and sends POST request to IBM Watson, returns JSON response 
 	def request(self,audio_format):
 		url = "https://api.au-syd.speech-to-text.watson.cloud.ibm.com/instances/8d79c68b-01d0-4fbd-8ef7-c63817d41397/v1/recognize?speaker_labels=true"
 		auth = ('apikey', '8ecjDiukKvweVEqbJfGflJ1bQH66eYIQelXxeHoZLIk5')
@@ -49,9 +50,23 @@ class Transcribe:
 		except Exception as err:
 		    print(f'Other error occurred: {err}')
 
-	def printResponse():
-		pass
+	# Prints the desired Output -- format "Person Number - time-time" --example "Person 1 - 1:00-1:52" 
+	def printResponse(self, response):
+		response_speakers = response['speaker_labels']
+		speakers = set()
+		for i in range(len(response_speakers)):
 
+			if (response_speakers[i]['speaker'] + 1) not in speakers:
+				if i == 0:
+					speakers.add(response_speakers[i]['speaker'] + 1)
+					print('Person ' + str(response_speakers[i]['speaker'] + 1) + ' - ' + str(response_speakers[i]['from']).replace('.',':') + '-', end = "")
+				else:
+					speakers.add(response_speakers[i]['speaker'] + 1)
+					print(str(response_speakers[i-1]['to']).replace('.',':'))
+					print('Person ' + str(response_speakers[i]['speaker'] + 1) + ' - ' + str(response_speakers[i]['from']).replace('.',':') + '-', end = "")
+			
+			elif response_speakers[i]['final'] == True:
+					print(str(response_speakers[i-1]['to']).replace('.',':'))
 
 if __name__ == "__main__":
 	file_name = sys.argv[1]
@@ -65,7 +80,7 @@ if __name__ == "__main__":
 	response = obj.request(audio_format)
 	# print(response)
 
-	#obj.printResponse(response)
+	obj.printResponse(response)
 
 
 
